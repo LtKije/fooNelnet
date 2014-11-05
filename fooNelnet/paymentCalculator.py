a67a1ef71fdd1b51f5c7f2bb51d7f1fa5ead88d5
LtKije <boomcallister@gmail.com>
2014-11-05 06:45:59 -0800

    added default payment in case we have to payment history

--- fooNelnet/paymentCalculator.py
+++ fooNelnet/paymentCalculator.py
@@ -1,19 +1,25 @@
+import os.path
+
 import datetime
 from userInfo import PAYMENT_CSV_FILE_NAME 
 from userInfo import MONTHLY_PAYMENT_AMOUNT 
 
 def calculateNextPayment():
     #get the date of our last payment
-    fh = open(PAYMENT_CSV_FILE_NAME, 'rb')
-    first = fh.readline()
-    fh.seek(-2, 2)
-    while fh.read(1) != "\n":
-        fh.seek(-2, 1)
-    last = fh.readline()
-    lastDate = datetime.datetime.strptime(last.split(',')[0], "%Y/%m/%d %H:%M:%S")
-    currentDate = datetime.datetime.now()
-    delta = currentDate - lastDate
-    return round((MONTHLY_PAYMENT_AMOUNT * 12.0 / 365.0) * delta.days, 2)
+    num_days = 1
+    if os.path.isfile(CSV_FILE_NAME):
+        fh = open(PAYMENT_CSV_FILE_NAME, 'rb')
+        first = fh.readline()
+        fh.seek(-2, 2)
+        while fh.read(1) != "\n":
+            fh.seek(-2, 1)
+        last = fh.readline()
+        lastDate = datetime.datetime.strptime(last.split(',')[0], "%Y/%m/%d %H:%M:%S")
+        currentDate = datetime.datetime.now()
+        delta = currentDate - lastDate
+        num_days = delta.days
+        
+    return round((MONTHLY_PAYMENT_AMOUNT * 12.0 / 365.0) * num_days, 2)
     
     
    
